559fd6d46ef595e230a0ba8a925a579ff989a83f
NMF Wasi <wasi7741@gmail.com>
2025-07-18 04:18:31 +0900

    fixed home page

--- event_management/urls.py
+++ event_management/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.shortcuts import redirect
+
 from django.contrib import admin
 from django.urls import path, include
-from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', lambda request: redirect('organizer_dashboard')),
     path('event/', include("event.urls")),
-]+debug_toolbar_urls()
+]
