05973550192b850ca80d0d4d921f9fce343aab1c
NMF Wasi <wasi7741@gmail.com>
2025-07-18 04:05:30 +0900

    Ready for render

--- event_management/settings.py
+++ event_management/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -94,15 +94,23 @@ WSGI_APPLICATION = "event_management.wsgi.application"
 #     }
 # }
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "event-management",
+#         "USER": "postgres",
+#         "PASSWORD": "1234",
+#         "HOST": "localhost",
+#         "PORT": "5432",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "event-management",
-        "USER": "postgres",
-        "PASSWORD": "1234",
-        "HOST": "localhost",
-        "PORT": "5432",
-    }
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://event_management_hzy6_user:In0kg7TgHeS1Pzo9TACkP0zk8pnVWlHB@dpg-d1skaa7gi27c739h3in0-a.oregon-postgres.render.com/event_management_hzy6',
+        conn_max_age=600
+    )
 }
 
 # Password validation
